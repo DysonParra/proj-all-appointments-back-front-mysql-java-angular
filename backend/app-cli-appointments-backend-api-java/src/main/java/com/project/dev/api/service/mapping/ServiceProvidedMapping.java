2b31dc94e0d1548aaa4230871a8438ff929d424c
unknown <dysontilano@gmail.com>
2025-02-23 23:29:56 -0500

    Min java version updated in javadoc

--- backend/app-cli-appointments-backend-api-java/src/main/java/com/project/dev/api/service/mapping/ServiceProvidedMapping.java
+++ backend/app-cli-appointments-backend-api-java/src/main/java/com/project/dev/api/service/mapping/ServiceProvidedMapping.java
@@ -23,7 +23,7 @@ import org.mapstruct.Mapping;
  * TODO: Description of {@code ServiceProvidedMapping}.
  *
  * @author Dyson Parra
- * @since 11
+ * @since Java 17 (LTS), Gradle 7.3
  */
 @Mapper(componentModel = "spring") //, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ServiceProvidedMapping extends GenericMapping<ServiceProvidedDTO, ServiceProvided> {
--- backend/app-cli-appointments-backend-api-java/src/main/java/com/project/dev/api/web/rest/assembler/EmployeeRestAssembler.java
+++ backend/app-cli-appointments-backend-api-java/src/main/java/com/project/dev/api/web/rest/assembler/EmployeeRestAssembler.java
@@ -25,7 +25,7 @@ import org.springframework.stereotype.Component;
  * TODO: Description of {@code EmployeeRestAssembler}.
  *
  * @author Dyson Parra
- * @since 11
+ * @since Java 17 (LTS), Gradle 7.3
  */
 @Component
 public class EmployeeRestAssembler implements RepresentationModelAssembler<EmployeeDTO, EntityModel<EmployeeDTO>> {
